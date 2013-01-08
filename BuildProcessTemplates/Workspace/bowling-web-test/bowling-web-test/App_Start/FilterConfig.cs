bb1db9531760aaf1adb8b3a0657d3469a6fe1f8b
Dan True <B45X\S082924>
2013-01-08 10:50:59 +0100

    testing out project creation in TFS

---
+++ BuildProcessTemplates/Workspace/bowling-web-test/bowling-web-test/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace bowling_web_test
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ BuildProcessTemplates/Workspace/bowling-web-test/bowling-web-test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bowling_web_test.MvcApplication" Language="C#" %>
