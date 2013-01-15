0fd912f449bda4ed153744659d10f02fde0f0572
Nicklas Laine Overgaard <B45X\S072423>
2013-01-15 12:45:43 +0100

    Added missing files

---
+++ src/bowling.entity.nhibernateprovider/NHibernateInitializer.cs
@@ -0,0 +1,45 @@
+﻿using Bowling.Entity.Domain;
+using NHibernate.Bytecode;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Mapping.ByCode;
+using SharpLite.NHibernateProvider;
+using SharpLite.NHibernateProvider.ConfigurationCaching;
+
+namespace TemplateSrc.NHibernateProvider
+{
+    public class NHibernateInitializer
+    {
+        public static Configuration Initialize() {
+            INHibernateConfigurationCache cache = new NHibernateConfigurationFileCache();
+
+            var mappingAssemblies = new[] { 
+                typeof(Member).Assembly.GetName().Name
+            };
+
+            var configuration = cache.LoadConfiguration(CONFIG_CACHE_KEY, null, mappingAssemblies);
+
+            if (configuration == null) {
+                configuration = new Configuration();
+
+                configuration
+                    .Proxy(p => p.ProxyFactoryFactory<DefaultProxyFactoryFactory>())
+                    .DataBaseIntegration(db => {
+                        db.ConnectionStringName = "TemplateSrcConnectionString";
+                        db.Dialect<SQLiteDialect>();
+                    })
+                    .AddAssembly(typeof(Member).Assembly)
+                    .CurrentSessionContext<LazySessionContext>();
+
+                //var mapper = new ConventionModelMapper();
+                //mapper.WithConventions(configuration);
+                
+                cache.SaveConfiguration(CONFIG_CACHE_KEY, configuration);
+            }
+
+            return configuration;
+        }
+
+        private const string CONFIG_CACHE_KEY = "TemplateSrc";
+    }
+}
