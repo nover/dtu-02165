1c0b9ce2d391f409f3cbae8971d38082ebf05901
Georgios Liassas <B45X\S114414>
2013-01-15 16:19:59 +0100

    MemberLogin and MemberLoginResponse DTOs added

---
+++ src/bowling.rest.servicemodel/Operations/MembersLogin.cs
@@ -0,0 +1,16 @@
+﻿using ServiceStack.ServiceHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling.Rest.Service.Model.Operations
+{
+    [Route("/members_login")]
+    class MembersLogin
+    {
+        public String Email { get; set; }
+        public String password { get; set; }
+    }
+}
---
+++ src/bowling.rest.servicemodel/Operations/MembersLoginResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling.Rest.Service.Model.Operations
+{
+    class MembersLoginResponse
+    {
+        public String Title { get; set; }
+        public String Name { get; set; }
+    }
+}
