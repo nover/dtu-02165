d637ae1cae2ae129ee594925cd4de698750a2bb6
Nicklas Laine Overgaard <B45X\S072423>
2013-01-18 11:38:13 +0100

    Added view and model for beginning a resevation. Added link from front-page

---
+++ src/bowling.website/Controllers/ReservationBeginController.cs
@@ -0,0 +1,21 @@
+﻿using Bowling.Web.CustomerSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bowling.Web.CustomerSite.Controllers
+{
+    public class ReservationBeginController : Controller
+    {
+        //
+        // GET: /ReservationBegin/
+
+        public ActionResult Index()
+        {
+            return View(new ReservationBeginInputModel());
+        }
+
+    }
+}
---
+++ src/bowling.website/Models/ReservationBeginInputModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bowling.Web.CustomerSite.Models
+{
+    public class ReservationBeginInputModel
+    {
+        [Required]
+        public int NumberOfPeople { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PlayAt { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan StartPlayingAt { get; set; }
+
+        [Required]
+        public TimeSpan StopPlayingAt { get; set; }
+    }
+}
