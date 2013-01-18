d1419a49e5c662ceee57361f45f39fb745c5491b
Nicklas Laine Overgaard <B45X\S072423>
2013-01-18 22:43:44 +0100

    Created test to show that impossible reservation should not be allowed. However, something seems to be off in regards to the scheduler integration as it allows the reservation.

--- src/bowling.rest.tests/Services/ReservationPossibleServiceTest.cs
+++ src/bowling.rest.tests/Services/ReservationPossibleServiceTest.cs
@@ -65,5 +65,41 @@ namespace bowling.rest.tests.Services
 			ReservationPossibleResponse response = service.OnGet(request) as ReservationPossibleResponse;
 			Assert.That(response.IsPossible, Is.True);
 		}
+		[TestMethod]
+		public void TestImpossibleReservationsDueToLackOfTimeSlots()
+		{
+			for (int i = 0; i < this.lanes.Count; i++)
+			{
+				var reservation = new Reservation()
+				{
+					CreatedAt = DateTime.Now,
+					Name = "Gert",
+					NumberOfPlayers = 4,
+					PhoneNumber = "1234",
+					PlayAt = DateTime.Now
+				};
+
+				reservation.AddLane((from y in this.lanes select y).First<Lane>());
+				reservation.AddTimeSlot((from y in this.timeSlots select y).First<TimeSlot>());
+
+				this._session.Save(reservation);
+			}
+			// since we filled out all lanes at that timeslot, the next booking for that timeslot should not be possible
+			var service = new ReservationPossibleService();
+			var request = new ReservationPossible()
+			{
+				Reservation = new ReservationType()
+				{
+					HowManyHours = 2,
+					NumberOfPlayers = 8,
+					PlayAt = DateTime.Now,
+					TimeOfDay = (from y in this.timeSlots select y).First<TimeSlot>().Start
+				}
+			};
+
+			ReservationPossibleResponse response = service.OnGet(request) as ReservationPossibleResponse;
+			Assert.That(response.IsPossible, Is.False);
+
+		}
 	}
 }
