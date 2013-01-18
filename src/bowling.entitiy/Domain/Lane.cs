f02a6c75e93c1e49e7f1893bdc9d7eab92113d79
Georgios Liassas <B45X\S114414>
2013-01-18 11:23:17 +0100

    Lane and TimeSlot entities added in the model

---
+++ src/bowling.entitiy/Domain/Lane.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling.Entity.Domain 
+{
+    public class Lane : SharpLite.Domain.Entity
+    {
+        public int Number { get; set; }
+    }
+}
---
+++ src/bowling.entitiy/Domain/TimeSlot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling.Entity.Domain 
+{
+    public class TimeSlot : SharpLite.Domain.Entity
+    {
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+    }
+}
