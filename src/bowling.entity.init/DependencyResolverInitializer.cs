5b568cc634e1ffad3869d2009207656d281713c6
Nicklas Laine Overgaard <B45X\S072423>
2013-01-18 21:39:57 +0100

    Created Local session initalizer for use in unit-tests

--- src/bowling.entity.init/DependencyResolverInitializer.cs
+++ src/bowling.entity.init/DependencyResolverInitializer.cs
@@ -12,31 +12,32 @@ using StructureMap.ServiceLocatorAdapter;
 
 namespace TemplateSrc.Init
 {
-    public class DependencyResolverInitializer
-    {
-        public static void Initialize() {
-            _container = new Container(x =>
-            {
-                x.For<ISessionFactory>()
-                    .Singleton()
-                    .Use(() => NHibernateInitializer.Initialize().BuildSessionFactory());
-                x.For<IEntityDuplicateChecker>().Use<EntityDuplicateChecker>();
-                x.For(typeof(IRepository<>)).Use(typeof(Repository<>));
-                x.For(typeof(IRepositoryWithTypedId<,>)).Use(typeof(RepositoryWithTypedId<,>));
-            });
-            
-            IDependencyResolver drStructureMap = new StructureMapDependencyResolver(_container);
-            DependencyResolver.SetResolver(drStructureMap);
-            var smServiceLocator = new StructureMapServiceLocator(_container);
-            ServiceLocator.SetLocatorProvider(() => smServiceLocator);
+	public class DependencyResolverInitializer
+	{
+		public static void Initialize()
+		{
+			_container = new Container(x =>
+			{
+				x.For<ISessionFactory>()
+					.Singleton()
+					.Use(() => NHibernateInitializer.Initialize().BuildSessionFactory());
+				x.For<IEntityDuplicateChecker>().Use<EntityDuplicateChecker>();
+				x.For(typeof(IRepository<>)).Use(typeof(Repository<>));
+				x.For(typeof(IRepositoryWithTypedId<,>)).Use(typeof(RepositoryWithTypedId<,>));
+			});
 
-        }
+			//IDependencyResolver drStructureMap = new StructureMapDependencyResolver(_container);
+			//DependencyResolver.SetResolver(drStructureMap);
+			var smServiceLocator = new StructureMapServiceLocator(_container);
+			ServiceLocator.SetLocatorProvider(() => smServiceLocator);
 
-        public static void AddDependency(Type pluginType, Type concreteType)
-        {
-            _container.Configure(x => x.For(pluginType).Use(concreteType));
-        }
+		}
 
-        private static Container _container;
-    }
+		public static void AddDependency(Type pluginType, Type concreteType)
+		{
+			_container.Configure(x => x.For(pluginType).Use(concreteType));
+		}
+
+		private static Container _container;
+	}
 }
---
+++ src/bowling.entity.init/LocalSessionInitalizer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using NHibernate;
+using SharpLite.NHibernateProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateSrc.NHibernateProvider
+{
+	/// <summary>
+	/// Nhibernate session provider for non web projects
+	/// </summary>
+	public static class LocalSessionInitializer
+	{
+		public static ISessionFactory Initialize()
+		{
+			try
+			{
+				ISessionFactory factory = ServiceLocator.Current.GetInstance<ISessionFactory>();
+				LazySessionContext.Bind(new Lazy<ISession>(() => BeginSession(factory)), factory);
+				return factory;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		private static ISession BeginSession(ISessionFactory sessionFactory)
+		{
+			var session = sessionFactory.OpenSession();
+			session.BeginTransaction();
+			return session;
+		}
+	}
+}
