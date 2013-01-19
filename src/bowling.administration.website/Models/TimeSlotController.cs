9e65b88c99db5837e1b621a144cd247b82abb184
Nicklas Laine Overgaard <B45X\S072423>
2013-01-19 16:36:50 +0100

    Added administration system and unittests. Its based on the Twitter.Bootstrap sample application

---
+++ src/bowling.administration.website/Models/TimeSlotController.cs
@@ -0,0 +1,27 @@
+﻿using BootstrapMvcSample.Controllers;
+using bowling.administration.website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bowling.administration.website.Controllers
+{
+    public class TimeSlotController : BootstrapBaseController
+    {
+        //
+        // GET: /TimeSlot/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+		public ActionResult Create()
+		{
+			return View(new TimeSlotInputModel());
+		}
+	}
+}
---
+++ src/bowling.administration.website/Models/TimeSlotInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace bowling.administration.website.Models
+{
+	public class TimeSlotInputModel
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[DataType(DataType.Time)]
+		public TimeSpan Start { get; set; }
+
+		[Required]
+		[DataType(DataType.Time)]
+		public TimeSpan End { get; set; }
+	}
+}
