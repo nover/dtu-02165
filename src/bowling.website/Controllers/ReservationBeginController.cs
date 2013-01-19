8eb3b648b13aca7b9d8d93d1f09402080ce01c9a
Nicklas Laine Overgaard <B45X\S072423>
2013-01-19 16:17:42 +0100

    Started working on the API integration for the reservation UI

--- src/bowling.website/Controllers/ReservationBeginController.cs
+++ src/bowling.website/Controllers/ReservationBeginController.cs
@@ -1,5 +1,7 @@
 ﻿using BootstrapMvcSample.Controllers;
+using Bowling.Rest.Service.Model.Operations;
 using Bowling.Web.CustomerSite.Models;
+using ServiceStack.ServiceClient.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,25 @@ namespace Bowling.Web.CustomerSite.Controllers
         {
             if (ModelState.IsValid)
             {
+				try
+				{
+					var response = this.CurrentAPIClient.Get<ReservationPossibleResponse>(
+						String.Format(
+							"/reservation/possible?NumberOfPlayers={0}&PlayAt={1}&TimeOfDay={2}HowManyHours={3}",
+							model.NumberOfPeople,
+							model.PlayAt,
+							model.StartPlayingAt,
+							model.HowManyHours
+						));
+
+
+				}
+				catch (WebServiceException ex)
+				{
+					Error("Awww, something terrible happened. The Leprechauns are working on the problem se please try again later.");
+					return View(model);
+				}
+
                 throw new NotImplementedException("Proceeding to next step is not supported yet");
             }
 
