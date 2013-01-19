65246318500b95d8a1cf0dc8c262e1517b763154
Nicklas Laine Overgaard <B45X\S072423>
2013-01-19 16:53:50 +0100

    List and creation of timeslots now works like a charm

--- src/bowling.administration.website/Models/TimeSlotController.cs
+++ src/bowling.administration.website/Models/TimeSlotController.cs
@@ -24,6 +24,31 @@ namespace bowling.administration.website.Controllers
             return View(slots);
         }
 
+		[HttpPost]
+		public ActionResult Create(TimeSlotInputModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var toSave = Mapper.Map<TimeSlot>(model);
+				var repos = DependencyResolver.Current.GetService<IRepository<TimeSlot>>();
+				try
+				{
+					repos.SaveOrUpdate(toSave);
+					repos.DbContext.CommitChanges();
+				}
+				catch (Exception ex)
+				{
+					Error("Something went horribly wrong while saving your changes<br/>Technical stuff: " + ex.Message);
+					return View(model);
+				}
+
+				Success("Your new time slot was saved");
+				return RedirectToAction("Index");
+			}
+
+			Error("Darn, your input was incorrect. Please try again");
+			return View(model);
+		}
 
 		public ActionResult Create()
 		{
